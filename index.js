96814a9048195f2469dfe4dc84b89bb69bec6e44
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 18:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name){ 
   drivers.push(name)
 }
-function destructivelyPrependDriver(prepend){ 
-  prepend === drivers
-  prepend.unshift("Bob")}
-function destructivelyRemoveLastDriver(removeLast) {
-  removeLast === drivers
-  removeLast.splice(2)}
+function destructivelyPrependDriver(name){ 
+  drivers.unshift(name)}
+function destructivelyRemoveLastDriver() {
+  drivers.splice(2)}
 function destructivelyRemoveFirstDriver(removeFirst){
   removeFirst === drivers
   removeLast.
