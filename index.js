7687c66bd854c22ee8ee6b5006ff1dc47025122f
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 18:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(){ drivers.push("Ralph")}
-function destructivelyPrependDriver(){ drivers.}
+function destructivelyPrependDriver(){ drivers.unshift()}
