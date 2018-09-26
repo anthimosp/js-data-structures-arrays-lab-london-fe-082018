2b5da01cd798dfd9216452f015411384a22ff83f
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 18:08:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendDriver(append){ drivers.push("Ralph")}
+function destructivelyAppendDriver(append){ 
+  drivers.push("Ralph")}
 function destructivelyPrependDriver(prepend){ drivers.unshift("Bob")}
-function destructivelyRemoveLastDriver(removeLast) {drivers.OfSh}
+function destructivelyRemoveLastDriver(removeLast) {drivers.indexOf()}
