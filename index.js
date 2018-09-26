f6d9dd304091a1b32ef6e206f6cb41743cfbcb64
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 18:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function appendDriver(name){
 function prependDriver(name){
   return [name, ...drivers]}
 function removeLastDriver(){
-  return drivers.slice(3)}
+  return drivers.slice(2)}
 function removeFirstDriver(){
   drivers.shift(0)}
