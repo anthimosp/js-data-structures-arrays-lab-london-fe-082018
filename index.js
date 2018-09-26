cb4448dbf28b5931ae5fd35eb95f10e24b0e3219
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 18:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(){ drivers.push("Ralph")}
 function destructivelyPrependDriver(){ drivers.unshift("Bob")}
+function destructivelyRemoveLastDriver()
